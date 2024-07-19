c9cb00c95cc79a7bb3c6f05ffdda2993ca081452
Negus <natnaelbirhanu22@gmail.com>
2024-07-19 12:10:46 +0300

    pulse animation

--- src/components/default-template/DefaultProjectsSection/ProjectItem/ProjectItemTech.tsx
+++ src/components/default-template/DefaultProjectsSection/ProjectItem/ProjectItemTech.tsx
@@ -3,8 +3,8 @@ import type { ProjectTech } from "constants/projects";
 
 export const ProjectItemTech = ({ tech, icon: Icon }: ProjectTech) => {
   return (
-    <li className="flex gap-[6px] align-center min-w-0 bg-primary/50 py-[6px] px-[12px] rounded-[20px]  hover:animate-ping  transition duration-150 ease-in-out">
-      <Icon className="w-[14px] h-[14px]"  />
+    <li className="flex gap-[6px] align-center min-w-0 bg-primary/50 py-[6px] px-[12px] rounded-[20px]  hover:animate-pulse  transition duration-300 ease-in-out">
+      <Icon className="w-[14px] h-[14px]" />
       <Typography className="text-xs truncate">{tech}</Typography>
     </li>
   );
